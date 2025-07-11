c0df49d22753a095da792c20208960231983347d
Adnilson Moraes <progamador0001@gmail.com>
2025-07-11 14:05:05 -0300

    11 de Julho

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const apiKeyInput = document.getElementById('apiKey');
-const gameSelect = document.getElementById('gameSelect');
-const questionInput = document.getElementById('questionInput');
-const askButton = document.getElementById('askButton');
-const aiResponse = document.getElementById('aiResponse');
-const form = document.getElementById('form');
+const apiKeyInput = document.getElementById('apiKey')
+const gameSelect = document.getElementById('gameSelect')
+const questionInput = document.getElementById('questionInput')
+const askButton = document.getElementById('askButton')
+const aiResponse = document.getElementById('aiResponse')
+const form = document.getElementById('form')
 
 const enviarFormulario = (event) => {
-    event.preventDefault();
-    console.log(event)
+    event.preventDefault()
+    const apiKey = apiKeyInput.value
+    const game = gameSelect.value
+    const question = questionInput.value
+
+    console.log({apiKey, game, question})
+
+     if(apiKey == '' || game == '' || question == '') {
+        alert('Por favor, preencha todos os campos.')
+        return
+
+     }
 
 }
 form.addEventListener('submit', () => {})
